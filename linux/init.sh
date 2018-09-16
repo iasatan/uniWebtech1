de7c968b84789f502af9e7b9875e0434136f929e
Sátán Ádám <satanadam@outlook.com>
2018-09-16 19:18:38 +0200

    Create init.sh

---
+++ linux/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt install nodejs
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+apt-get update
+apt-get install sublime-text
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+tar -xzf postman.tar.gz -C /opt
+rm postman.tar.gz
+ln -s /opt/Postman/Postman /usr/bin/postman
+apt install htop wireshark ruby-sass apache2 firefox git
+update-rc.d apache2 defaults
